43e20663773fce3a9f28e2869614fbaf225edf65
Winstelz <Winstelz@gmail.com>
2024-09-23 08:52:46 -0400

    Add files via upload

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -6,6 +6,8 @@ export class Home {
     readonly Submit: Locator;
     readonly Shop: getbyRole;
     readonly FreshPicked: Locator;
+    readonly Learn: getbyRole;
+    readonly PageComing: Locator;
     
 
     constructor(page) {
@@ -14,6 +16,9 @@ export class Home {
         this.Submit = page.getByRole('button')
         this.Shop = page.getByRole('button', { name: 'Shop' })
         this.FreshPicked = page.getByRole('navigation').getByRole('link', { name: 'Fresh Picked' })
+        this.Learn = page.getByRole('button', { name: 'Learn' })
+        this.PageComing = page.locator("//a[normalize-space()='Page coming soon']")
+
 
     }
 
@@ -37,7 +42,15 @@ export class Home {
         await this.page.waitForTimeout(2000);
         expect(this.page.url()).toContain('/products/fresh-picked?Size=6');
     }
-
+    async ClickLearn () {
+        await this.Learn.click();
+        await this.page.waitForTimeout(2000);
+    }
+    async ClickPageComingSoon () {
+        await this.PageComing.click();
+        await this.page.waitForTimeout(2000);
+        expect(this.page.url()).toContain("/products/fresh-picked?Size=6");
+    }
 
 
 }
