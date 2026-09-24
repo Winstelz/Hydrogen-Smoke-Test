ff7f062d1ecb43af016645e3d3108d7534b88e80
Winstelz <Winstelz@gmail.com>
2026-09-24 06:41:38 -0400

    Locator/Button update

--- src/pom/homePage.ts
+++ src/pom/homePage.ts
@@ -100,8 +100,8 @@ export class HomePage {
         this.shopLookImage2 = page.getByTestId('image-wrapper').locator('img[alt="FizzBlend Classic Tee!"]');
         this.shopLookButton3 = page.getByRole('button', { name: 'Fundays Yellow' });
         this.shopLookImage3 = page.getByTestId('image-wrapper').locator('img[alt="Skinny Dip"]');
-        this.startBrowsingButton = page.getByRole('button', { name: 'Start Browsing'});
-        this.findHelpButton = page.getByRole('button', { name: 'Find Help'});
+        this.startBrowsingButton = page.getByRole('link', { name: 'Start Browsing'});
+        this.findHelpButton = page.getByRole('link', { name: 'Find Help'});
     
     }
 
